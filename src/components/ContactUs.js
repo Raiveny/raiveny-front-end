48109d91de4c496227f7baa3181ab1ca9e37c3b6
Alhaimouni <m.alhaumouni@gmail.com>
2022-08-15 21:02:28 +0300

    edit aboutUs

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './ContactUsCSS.css';
-import ContactUsImg from './assets/ContactUsImg.jpg'
 import emailjs from 'emailjs-com'
-
-// import '/home/fatimakh/mid-project-301/raiveny-front-end/src/App.css';
-// import Test from './components/test';
+import 'font-awesome/css/font-awesome.min.css';
 class ContactUs extends Component {
     render() {
         function sendEmail(e) {
             e.preventDefault() // prevents the default behaviour of the form
-
             emailjs.sendForm('gmail', 'template_u8xmz7r',
                 e.target, 'oXDGlQ6ZHy4kwz4DD')
                 .then((result) => {
@@ -25,49 +18,46 @@ class ContactUs extends Component {
         return (
             <div className='container'>
                 <h1> Contact Us </h1>
-                {/* <title className='contactus_title'> Contact Us</title> */}
-                <div className='container-img_form'>
-                    <div className='img'> <img src="https://i.postimg.cc/bwwN8FVL/imageedit-11-8517654771.png" alt='' width="500" height="500" /></div>
-                    <div className='form'>
-
-                        <Form onSubmit={sendEmail}>
-                            <p className='Contact-p'>You’ve Got Questions for Us? Kindly Let Us Know Below</p>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Full Name</Form.Label>
-                                <br />
-                                <Form.Control type="text" id="name" placeholder="Enter your name ..." />
-                            </Form.Group>
-
-                            <Form.Group className="mb-3" >
-                                <Form.Label>Email</Form.Label>
-                                <br />
-
-                                <Form.Control type="email" id="email" placeholder="Enter your email ..." />
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Subject</Form.Label>
-                                <br />
-
-                                <Form.Control type="text" id="subject" placeholder="Subject" />
-                            </Form.Group>
-
-                            <Form.Group className="mb-3">
-                                <Form.Label>Message</Form.Label>
-                                <br />
-
-                                <textarea name="message" rows="5" cols="50" placeholder='Write here ...'></textarea>
-                            </Form.Group>
-                            <div>
-                                <Button variant="primary" type="submit" className='send-rest-contract-us-buttons' >
-                                    Send
-                                </Button>
+                <div className='content'>
+                    <div className='left-side' >
+                        <div className='address details' >
+                            <i className="fas fa-map-marker-alt" > </i>
+                            <div className='topic'>Address</div>
+                            <div className='text-one'> Irbid, Jordan</div>
+                            <div className='text-two'> LTUC </div>
+                        </div>
+                        <div className="Phone details">
+                            <i className="fas fa-phone-alt"></i>
+                            <div className='topic'>Phone</div>
+                            <div className='text-one'>++962789058158 </div>
+                            <div className='text-two'>++962787505333 </div>
+                        </div>
+                        <div className="email details">
+                            <i className="fas fa-envelope"></i>
+                            <div className='topic'>Email</div>
+                            <div className='text-one'>team301project@gmail.com</div>
+                        </div>
+                    </div>
+                    <div className='right-side'>
+                        <div className='topic-text' >Send us a message</div>
+                        <p> Send a message and we will get back to you within 24 hours</p>
+                        <form onSubmit={sendEmail} action='#' >
+                            <div className='input-box'>
+                                <input type="text" placeholder='Enter your name' ></input>
                             </div>
-                        </Form>
+                            <div className='input-box'>
+                                <input type="text" placeholder='Enter your emai' ></input>
+                            </div>
+                            <div className='input-box message-box'>
+                                <textarea></textarea>
+                            </div>
+                            <div className='button'>
+                                <button className='button'>Send</button>                                </div>
+                        </form>
                     </div>
                 </div>
             </div>
         );
     }
 }
-
 export default ContactUs;
