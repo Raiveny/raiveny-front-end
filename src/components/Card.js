5ae98a0fd2faa1a179a72622e2a4d75630894e67
Hassanabdelqader <terawihassan@gmail.com>
2022-08-15 22:34:51 +0300

    Updte

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,13 +11,19 @@ class BasicExample extends React.Component {
             return (
                 <div className='cardClass'>
                 <Card style={{ width: '18rem' }} className='cardParent' >
-                        <Card.Img  className='cardImg' variant="top" src={(this.props.data.medication_img)?this.props.data.medication_img:`https://thumbs.dreamstime.com/b/no-image-available-icon-photo-camera-flat-vector-illustration-132483141.jpg`} />
+                <Card.Img  className='cardImg' variant="top" src={(this.props.data.medication_img)?this.props.data.medication_img:`https://thumbs.dreamstime.com/b/no-image-available-icon-photo-camera-flat-vector-illustration-132483141.jpg`}/>
                         <Card.Body className='cardBody' >
-                            <Card.Title className='cardTitle' >{(this.props.data!==undefined)?this.props.data.medication_Name:""}</Card.Title>
-                            <Card.Text className='cardText'>
-                            {(this.props.data)? `Dosage: `+this.props.data.medication_Dosage:""}
-                            </Card.Text>
+                        <Card.Title className='cardTitle' >{(this.props.data!==undefined)?this.props.data.medication_Name:""}</Card.Title>
+
+                            <div className='dosageDiv' >
+                                {this.props.data.medication_Dosage.map(v=>    
+                                   <Card.Text className='cardText'>
+                                              {(v)? v + ".":""}
+                                       </Card.Text>
+                                      )}
+                            </div>
                         </Card.Body>
+                       
                         </Card>
                 </div>
             );
@@ -25,3 +31,4 @@ class BasicExample extends React.Component {
 }
 
 export default BasicExample;
+
