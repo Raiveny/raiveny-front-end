03c3144593ad2fe5d1b93fd07b8ae6c20cff6381
Hassanabdelqader <terawihassan@gmail.com>
2022-08-17 00:26:46 +0300

    Edite Exp

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ class Header extends React.Component {
 
         return (
             <>
-                <Navbar collapseOnSelect expand="lg" style={{ backgroundColor: "#fff" }}>
+                <Navbar collapseOnSelect expand="md" style={{ backgroundColor: "#fff" }}>
              
                     <Navbar.Brand 
                     // href="/"
